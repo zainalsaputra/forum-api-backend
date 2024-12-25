6a5802b4b778570195ddd21d60a3c021d1ca79ad
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2024-12-25 23:30:49 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -47,6 +47,22 @@ describe('HTTP server', () => {
       payload: requestPayload,
     });
 
+    describe('when GET /', () => {
+      it('should return 200 and hello world', async () => {
+        // Arrange
+        const server = await createServer({});
+        // Action
+        const response = await server.inject({
+          method: 'GET',
+          url: '/',
+        });
+        // Assert
+        const responseJson = JSON.parse(response.payload);
+        expect(response.statusCode).toEqual(200);
+        expect(responseJson.value).toEqual('Hello world!');
+      });
+    });
+
     // Assert
     const responseJson = JSON.parse(response.payload);
     expect(response.statusCode).toEqual(500);
